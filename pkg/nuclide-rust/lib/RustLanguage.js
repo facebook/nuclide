0809729b15fc67e2df2a0d1b5f326c39a08962c7
Igor Matuszewski <Xanewok@gmail.com>
2018-08-10 16:58:38 +0200

    Clean up and add more LSP capabilities

--- pkg/nuclide-rust/lib/RustLanguage.js
+++ pkg/nuclide-rust/lib/RustLanguage.js
@@ -28,20 +28,25 @@ async function connectionToRustService(
     getNotifierByConnection(connection),
     getHostServices(),
   ]);
+  const service = getVSCodeLanguageServiceByConnection(connection);
+
   const cmd = 'rls';
-  const lspService = await getVSCodeLanguageServiceByConnection(
-    connection,
-  ).createMultiLspLanguageService('rust', cmd, [], {
-    fileNotifier,
-    host,
-    projectFileNames: ['Cargo.toml'],
-    fileExtensions: ['.rs'],
-    logCategory: 'nuclide-rust',
-    logLevel: 'TRACE',
-    useOriginalEnvironment: true,
-    additionalLogFilesRetentionPeriod: 5 * 60 * 1000, // 5 minutes
-    waitForDiagnostics: true,
-  });
+  const lspService = await service.createMultiLspLanguageService(
+    'rust',
+    cmd,
+    [],
+    {
+      fileNotifier,
+      host,
+      projectFileNames: ['Cargo.toml', '.buckconfig'],
+      fileExtensions: ['.rs'],
+      logCategory: 'nuclide-rust',
+      logLevel: 'TRACE',
+      useOriginalEnvironment: true,
+      additionalLogFilesRetentionPeriod: 5 * 60 * 1000, // 5 minutes
+      waitForDiagnostics: true,
+    },
+  );
 
   return lspService || new NullLanguageService();
 }
@@ -49,45 +54,53 @@ async function connectionToRustService(
 async function createLanguageService(): Promise<
   AtomLanguageService<LanguageService>,
 > {
-  // TODO: Fill in more, already supported features
-  const diagnosticsConfig = {
-    version: '0.2.0',
-    analyticsEventName: 'rust.observe-diagnostics',
-  };
-
-  const definitionConfig = {
-    version: '0.1.0',
-    priority: 1,
-    definitionEventName: 'rust.definition',
-  };
-
-  const typeHint = {
-    version: '0.0.0',
-    priority: 1,
-    analyticsEventName: 'ocaml.typeHint',
-  };
-
-  const autocompleteConfig = {
-    inclusionPriority: 1,
-    suggestionPriority: 3,
-    excludeLowerPriority: false,
-    analytics: {
-      eventName: 'nuclide-rust',
-      shouldLogInsertedSuggestion: false,
-    },
-    disableForSelector: null,
-    autocompleteCacherConfig: null,
-    supportsResolve: false,
-  };
-
   const atomConfig: AtomLanguageServiceConfig = {
     name: 'Rust',
     grammars: ['source.rust'],
-    diagnostics: diagnosticsConfig,
-    definition: definitionConfig,
-    autocomplete: autocompleteConfig,
-    typeHint,
+    diagnostics: {
+      version: '0.2.0',
+      analyticsEventName: 'rust.observe-diagnostics',
+    },
+    definition: {
+      version: '0.1.0',
+      priority: 1,
+      definitionEventName: 'rust.definition',
+    },
+    codeFormat: {
+      version: '0.1.0',
+      priority: 1,
+      analyticsEventName: 'rust.formatCode',
+      canFormatRanges: true,
+      canFormatAtPosition: true,
+    },
+    findReferences: {
+      version: '0.1.0',
+      analyticsEventName: 'rust.get-references',
+    },
+    rename: {
+      version: '0.0.0',
+      priority: 1,
+      analyticsEventName: 'rust:rename',
+    },
+    autocomplete: {
+      inclusionPriority: 1,
+      suggestionPriority: 3,
+      excludeLowerPriority: false,
+      analytics: {
+        eventName: 'nuclide-rust',
+        shouldLogInsertedSuggestion: false,
+      },
+      disableForSelector: '.source.rust .comment, .source.rust .string',
+      autocompleteCacherConfig: null,
+      supportsResolve: false,
+    },
+    typeHint: {
+      version: '0.0.0',
+      priority: 1,
+      analyticsEventName: 'rust.typeHint',
+    },
   };
+
   return new AtomLanguageService(connectionToRustService, atomConfig);
 }
 
